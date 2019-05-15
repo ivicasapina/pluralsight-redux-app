6ab1140e8b09df2f1e26543208cb6904690c17ba
Ivica Šapina <ivica.sapina@domnet.ba>
2019-05-15 15:05:16 +0200

    Modified course actions and reducers

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,6 +4,8 @@ function courseReducer(state = [], action) {
   switch (action.type) {
     case types.CREATE_COURSE:
       return [...state, { ...action.payload }];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.payload;
     default:
       return state;
   }
