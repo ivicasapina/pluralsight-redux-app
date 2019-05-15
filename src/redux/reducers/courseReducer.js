c1a7515a4290c6baf520782e55e21458183d5404
Ivica Šapina <ivica.sapina@domnet.ba>
2019-05-15 15:20:46 +0200

    Connected authors on courses load

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
-function courseReducer(state = [], action) {
+function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.CREATE_COURSE:
       return [...state, { ...action.payload }];
