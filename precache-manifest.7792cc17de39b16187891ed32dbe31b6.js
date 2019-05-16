605c6cc8973fdebf65285d132642ec126b9ec518
Ivica Šapina <ivica.sapina@domnet.ba>
2019-05-16 15:39:55 +0200

    Updates

---
+++ precache-manifest.7792cc17de39b16187891ed32dbe31b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d18f8903d6c072d28123bbbdac0bacd",
+    "url": "/pluralsight-cms-demo/index.html"
+  },
+  {
+    "revision": "0d1ef1a8fe6513f2fa88",
+    "url": "/pluralsight-cms-demo/static/css/2.4ed2986c.chunk.css"
+  },
+  {
+    "revision": "bc7c9008782b8149c250",
+    "url": "/pluralsight-cms-demo/static/css/main.cf5e52b8.chunk.css"
+  },
+  {
+    "revision": "0d1ef1a8fe6513f2fa88",
+    "url": "/pluralsight-cms-demo/static/js/2.368d8825.chunk.js"
+  },
+  {
+    "revision": "bc7c9008782b8149c250",
+    "url": "/pluralsight-cms-demo/static/js/main.0ffd50f4.chunk.js"
+  },
+  {
+    "revision": "c73bbad80ab1d1e68926",
+    "url": "/pluralsight-cms-demo/static/js/runtime~main.cf3c81af.js"
+  }
+]);
